3d26b730aad92f9c1ab684bf436cd70eb3abf56c
Dorian Morones <dorianm393@gmail.com>
2020-01-15 11:37:25 -0600

    Update Header.test.js

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import ProviderMock from '../../__mocks__/ProviderMock';
+import ProviderMock from '../../__mocks__/providerMock';
 import { create } from 'react-test-renderer';
 import Header from '../../components/Header';
 
